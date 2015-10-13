2ed361cb48ed01e085e457c1742dea1f82f7e4a5
Yolanda Robla <yolanda.robla-mota@hp.com>
2015-10-13 12:53:23 +0200

    Add mysql class to etherpad to optionally configure db
    Change-Id: Ib97188f3597fccf3d0a7b6f6e394ae6bacb12f65

---
+++ manifests/mysql.pp
@@ -0,0 +1,28 @@
+# == Class: puppet-etherpad_lite::mysql
+#
+class etherpad_lite::mysql(
+  $mysql_root_password,
+  $database_name = 'etherpad-lite',
+  $database_user = 'eplite',
+  $database_password,
+) {
+  class { '::mysql::server':
+    config_hash => {
+      'root_password'  => $mysql_root_password,
+      'default_engine' => 'InnoDB',
+      'bind_address'   => '127.0.0.1',
+    }
+  }
+
+  mysql::db { $database_name:
+    user     => $database_user,
+    password => $database_password,
+    host     => 'localhost',
+    grant    => ['all'],
+    charset  => 'utf8',
+    require  => [
+      Class['mysql::server'],
+      Class['mysql::server::account_security'],
+    ],
+  }
+}
