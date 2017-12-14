1ffb8fd8ad37294a5b656a78334e9b667d3eb96c
Ian Wienand <iwienand@redhat.com>
2017-12-15 10:51:49 +1100

    Add basic rspec test
    Add a basic deployment test
    
    Change-Id: I3ebec9b0c82a228f9d3a4e3d865ca44a233ec83e

---
+++ spec/acceptance/etherpad_lite_spec.rb
@@ -0,0 +1,18 @@
+require 'puppet-openstack_infra_spec_helper/spec_helper_acceptance'
+
+describe 'puppet-etherpad_lite:: manifest', :if => ['debian', 'ubuntu'].include?(os[:family]) do
+  def pp_path
+    base_path = File.dirname(__FILE__)
+    File.join(base_path, 'fixtures')
+  end
+
+  def init_puppet_module
+    module_path = File.join(pp_path, 'etherpad_lite.pp')
+    File.read(module_path)
+  end
+
+  it 'should work with no errors' do
+    apply_manifest(init_puppet_module, catch_failures: true)
+  end
+
+end
---
+++ spec/acceptance/fixtures/etherpad_lite.pp
@@ -0,0 +1,9 @@
+class { '::etherpad_lite':
+    ep_ensure      => 'latest',
+    eplite_version => 'cc9f88e7ed4858b72feb64c99beb3e13445ab6d9',
+    nodejs_version => 'system',
+}
+
+class { '::etherpad_lite::apache': }
+
+class { '::etherpad_lite::site': }
