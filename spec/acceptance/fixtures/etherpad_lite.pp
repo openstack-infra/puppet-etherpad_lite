5f667116d0b3cc1908efa0df91adfef240c6138b
Ian Wienand <iwienand@redhat.com>
2017-12-15 10:51:49 +1100

    Add basic rspec test
    Add a basic deployment test
    
    Change-Id: I3ebec9b0c82a228f9d3a4e3d865ca44a233ec83e

---
+++ spec/acceptance/fixtures/etherpad_lite.pp
@@ -0,0 +1,17 @@
+class { '::etherpad_lite':
+  ep_ensure      => 'latest',
+  eplite_version => 'cc9f88e7ed4858b72feb64c99beb3e13445ab6d9',
+  nodejs_version => 'system',
+}
+
+class { '::etherpad_lite::apache':
+  ssl_cert_file_contents => file('/etc/ssl/certs/ssl-cert-snakeoil.pem')
+  ssl_cert_file          => '/etc/pki/tls/certs/localhost.pem',
+  ssl_key_file_contents  => file('/etc/ssl/private/ssl-cert-snakeoil.key'),
+  ssl_key_file           => '/etc/pki/tls/private/localhost.key',
+}
+
+class { '::etherpad_lite::site':
+  database_password => 'fake_password',
+  etherpad_title    => 'A fake title',
+}
