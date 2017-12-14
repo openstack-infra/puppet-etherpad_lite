3cf0ae723b1e100b8bd9337b4ce0e15d97af522e
Ian Wienand <iwienand@redhat.com>
2017-12-15 10:51:49 +1100

    Add basic rspec test
    Add a basic deployment test
    
    Change-Id: I3ebec9b0c82a228f9d3a4e3d865ca44a233ec83e

---
+++ spec/acceptance/etherpad_lite_spec.rb
@@ -0,0 +1,63 @@
+require 'puppet-openstack_infra_spec_helper/spec_helper_acceptance'
+
+describe 'puppet-etherpad_lite:: manifest', :if => ['debian', 'ubuntu'].include?(os[:family]) do
+  def pp_path
+    base_path = File.dirname(__FILE__)
+    File.join(base_path, 'fixtures')
+  end
+
+  def preconditions_puppet_module
+    module_path = File.join(pp_path, 'preconditions.pp')
+    File.read(module_path)
+  end
+
+  before(:all) do
+    apply_manifest(preconditions_puppet_module, catch_failures: true)
+  end
+
+  def init_puppet_module
+    module_path = File.join(pp_path, 'etherpad_lite.pp')
+    File.read(module_path)
+  end
+
+  it 'should work with no errors' do
+    apply_manifest(init_puppet_module, catch_failures: true)
+  end
+
+  describe 'required files' do
+    describe file('/opt/etherpad-lite/') do
+      it { should be_directory }
+      it { should be_grouped_into 'eplite' }
+    end
+
+    # check service file installed
+    describe file('/etc/systemd/system/etherpad-lite.service') do
+      it { should be_file }
+    end
+
+    # check git got all the source
+    describe file('/opt/etherpad-lite/etherpad-lite/bin/installDeps.sh') do
+      it { should be_file }
+    end
+
+    # check npm modules installed
+    describe file('/home/eplite/.npm') do
+      it { should be_directory }
+    end
+  end
+
+  # This needs more work before it's up in the test env ...
+  #
+  # describe 'required services' do
+  #   describe 'ports are open and services are reachable' do
+  #     describe port(80) do
+  #       it { should be_listening }
+  #     end
+
+  #     describe command('curl -L -k http://localhost --verbose') do
+  #       its(:stdout) { should contain('randomPadName()') }
+  #     end
+  #   end
+  # end
+
+end
